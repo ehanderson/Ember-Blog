cf5f556e178d808847496d992f46ffec382c5f39
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-27 16:48:12 -0600

    Ive made a basic set up

---
+++ js/app.js
@@ -0,0 +1,29 @@
+App = Ember.Application.create();
+
+App.Router.map(function(){
+  this.resource('about');
+  this.resource('posts');
+  this.resource('post', {path: ':post_id'});
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function(){
+    return posts;
+  }
+});
+
+var posts = [{
+  id: '1',
+  title: 'My first attempt with Ember',
+  author: {name: 'emily'},
+  date: new Date('01-10-2014'),
+  excerpt: "Today I tried to understand ember by doing their todo tutorial.",
+  body: "Due to the face I did not have the starter kit installed, it did not work and it took time to figure out why!"
+}, {
+  id: '2',
+  title: 'My second attempt with Ember',
+  author: {name: 'emily'},
+  date: new Date('01-27-2014'),
+  excerpt: "Today I tried to understand ember by doing their blog tutorial.",
+  body: "I also learned about ember inspector. Ember makes things easy!"
+}]
