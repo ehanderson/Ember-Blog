d9dd3e17698b3eb2aadcca20e20af9283391bdfc
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-01-27 17:19:19 -0600

    adding editing

--- js/app.js
+++ js/app.js
@@ -2,14 +2,28 @@ App = Ember.Application.create();
 
 App.Router.map(function(){
   this.resource('about');
-  this.resource('posts');
+  this.resource('posts', function(){
   this.resource('post', {path: ':post_id'});
+
+  });
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function(){
     return posts;
   }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+    actions: {
+      edit: function(){
+        this.set('isEditing', true);
+      },
+      doneEditing: function(){
+        this.set('isEditing', false);
+      }
+    }
 });
 
 var posts = [{
